9f1c2a8b6228f8a8c64931c2fb00bb1a43d6583f
prudhveerp <prudhvee.raopalli@ellucian.com>
2020-07-07 17:25:51 +0530

    a

---
+++ src/main/java/com/main/Class1.java
@@ -0,0 +1,21 @@
+package com.main;
+
+public class Class1 
+{
+	public static String resource1 = "academic-credentials";
+	public String resource2 = "academic-levels";
+	private String resource3;
+	public String resource4 = "academic-programs";
+	public String resource5 = "acc-str-components";
+	public String resource6 = "acc-str-compo-values";
+	boolean abc;
+	public static void main(String[] args) 
+	{
+		System.out.println(resource1);
+	}
+	
+	public void test()
+	{
+		
+	}
+}
