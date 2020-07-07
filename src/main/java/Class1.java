fdc156c17c21d88098f15f1ad2420cc3c03570da
prithvi08242 <67908670+prithvi08242@users.noreply.github.com>
2020-07-07 17:03:13 +0530

    Update Class1.java

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -4,7 +4,7 @@ public class Class1
 	public static String resource1 = "academic-credentials";
 	public static String resource2 = "academic-levels";
 	public String resource3 = "academic-periods";
-	public String resource4 = "academic-programs";
+	public String resource4 ;
 	public String resource5 = "acc-str-components";
 	public String resource6 = "acc-str-compo-values";
 	
