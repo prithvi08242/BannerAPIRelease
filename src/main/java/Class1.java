2880ae3736af5e975f68818895ac6156a8ec91e2
prithvi08242 <67908670+prithvi08242@users.noreply.github.com>
2020-07-07 14:09:50 +0530

    Update Class1.java

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -2,7 +2,7 @@
 public class Class1 
 {
 	public static String resource1 = "academic-credentials";
-	public String resource2 = "academic-levels";
+	public static String resource2 = "academic-levels";
 	public String resource3 = "academic-periods";
 	public String resource4 = "academic-programs";
 	public String resource5 = "acc-str-components";
